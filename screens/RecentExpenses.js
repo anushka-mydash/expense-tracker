b5237f16c9538d1f1f30d7fcebf2a84859321819
Kumari Anushka <anushka@mydash.ai>
2022-09-25 02:17:12 +0530

    recent expenses added + context configured

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native'
+import { useContext } from 'react'
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+import { ExpensesContext } from '../store/expensesContext'
+import { getDateMinusDays } from '../utils/date';
 
 export default function RecentExpenses() {
+  const expensesContext = useContext(ExpensesContext);
+
+  const recentExpenses = expensesContext.expenses.filter(expense => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  })
+
   return (
-    <ExpensesOutput expensesPeriod='Last 7 Days' />
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 Days' />
   )
 }
--- store/expensesContext.js
+++ store/expensesContext.js
@@ -5,13 +5,13 @@ const DUMMY_EXPENSES = [
     id: 'e1',
     description: 'A pair of shoes',
     amount: 59.99,
-    date: new Date('2022-09-01')
+    date: new Date('2022-09-24')
   },
   {
     id: 'e2',
     description: 'A pair of trousers',
     amount: 89.29,
-    date: new Date('2022-01-05')
+    date: new Date('2022-09-21')
   },
   {
     id: 'e3',
